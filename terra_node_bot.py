8a09d766d0cdf5bb292ee1f393003993bd4b45df
Paul to the Plessing <paul.plessing@block42.tech>
2020-05-24 13:15:16 +0700

    adding and deleting button logic works

--- terra_node_bot.py
+++ terra_node_bot.py
@@ -3,6 +3,7 @@ import logging
 import re
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.error import BadRequest
 from telegram.ext import Updater, PicklePersistence, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, \
     CallbackContext, run_async
 
@@ -58,6 +59,8 @@ def start(update, context):
     Send start message and display action buttons.
     """
 
+    expect(want=None, user_data=context.user_data)
+
     # Start job for user
     if 'job_started' not in context.user_data:
         #context.job_queue.run_repeating(node_checks, interval=30, context={
@@ -73,6 +76,81 @@ def start(update, context):
     # Send message
     update.message.reply_text(text, parse_mode='markdown')
     show_home_menu_new_msg(update=update, context=context)
+
+
+@run_async
+def cancel(update, context):
+    """
+    Go back to home menu
+    """
+
+    expect(want=None, user_data=context.user_data)
+    show_home_menu_new_msg(update=update, context=context)
+
+
+@run_async
+def dispatch_query(update, context):
+    """
+    Call right function depending on the button clicked
+    """
+
+    query = update.callback_query
+    query.answer()
+    data = query.data
+
+    expect(want=None, user_data=context.user_data)
+    edit = True
+    call = None
+
+    if data == 'home':
+        call = show_home_menu_edit_msg
+    elif data == 'add_node':
+        call = add_node
+    elif re.match('node_details', data):
+        call = node_details
+    elif data == 'confirm_node_deletion':
+        call = confirm_node_deletion
+    elif data == 'delete_node':
+        call = delete_node
+    elif data == 'show_detail_menu':
+        call = show_detail_menu
+    else:
+        edit = False
+
+    # Catch any 'Message is not modified' error by removing the keyboard
+    if edit:
+        try:
+            context.bot.edit_message_reply_markup(reply_markup=None,
+                                                  chat_id=update.callback_query.message.chat_id,
+                                                  message_id=update.callback_query.message.message_id)
+        except BadRequest as e:
+            if 'Message is not modified' in e.message:
+                pass
+            else:
+                raise
+
+    if call:
+        return call(update, context)
+
+
+@run_async
+def plain_input(update, context):
+    """
+    Handle if the users sends a message
+    """
+    want = context.user_data['want'] if 'want' in context.user_data else None
+    if want == 'add_node':
+        expect(want=None, user_data=context.user_data)
+        return handle_add_node(update, context)
+
+
+@run_async
+def error(update, context):
+    """
+    Log error.
+    """
+
+    logger.warning('Update "%s" caused error: %s', update, context.error)
 
 
 def show_home_menu_new_msg(update, context):
@@ -95,21 +173,6 @@ def show_home_menu_edit_msg(update, context):
     text = 'Choose an address from the list below or add one:'
     query = update.callback_query
     query.edit_message_text(text, reply_markup=InlineKeyboardMarkup(keyboard))
-
-
-def get_home_menu_buttons(context):
-    """
-    Return Keyboard buttons for the home menu
-    """
-
-    keyboard = [[]]
-
-    for address in context.user_data['nodes'].keys():
-        keyboard.append([InlineKeyboardButton(address, callback_data='node_details-' + address)])
-
-    keyboard.append([InlineKeyboardButton('Add Node', callback_data='add_node')])
-
-    return keyboard
 
 
 def add_node(update, context):
@@ -140,43 +203,50 @@ def handle_add_node(update, context):
     return show_home_menu_new_msg(update=update, context=context)
 
 
-@run_async
-def dispatch_query(update, context):
+def node_details(update, context):
     """
-    Call right function depending on the button clicked
+    Show Detail Menu for the selected address
     """
 
     query = update.callback_query
-    query.answer()
-    data = query.data
 
-    call = None
-    if data == 'home':
-        call = show_home_menu_edit_msg
-    elif data == 'add_node':
-        call = add_node
+    address = query.data.split("-")[1]
+    context.user_data['selected_node_address'] = address
 
-    if call:
-        return call(update, context)
+    return show_detail_menu(update=update, context=context)
 
 
-@run_async
-def plain_input(update, context):
+def confirm_node_deletion(update, context):
     """
-    Handle if the users sends a message
-    """
-    want = context.user_data['want']
-    if want == 'add_node':
-        expect(want=None, user_data=context.user_data)
-        return handle_add_node(update, context)
-
-
-def error(update, context):
-    """
-    Log error.
+    Ask user if he is sure to remove the address
     """
 
-    logger.warning('Update "%s" caused error: %s', update, context.error)
+    address = context.user_data['selected_node_address']
+
+    keyboard = [[
+        InlineKeyboardButton('YES ✅', callback_data='delete_node'),
+        InlineKeyboardButton('NO ❌', callback_data='show_detail_menu')
+    ]]
+    text = '⚠️ Do you really want to remove the address from your monitoring list? ⚠️\n' + address
+
+    query = update.callback_query
+    query.edit_message_text(text, parse_mode='markdown', reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+def delete_node(update, context):
+    """
+    Remove address vom the monitoring list
+    """
+
+    query = update.callback_query
+    address = context.user_data['selected_node_address']
+
+    del context.user_data['nodes'][address]
+
+    text = "❌ Node address got deleted! ❌\n" + address
+    query.answer(text)
+    query.edit_message_text(text)
+    show_home_menu_new_msg(update=update, context=context)
 
 
 """
@@ -184,6 +254,41 @@ def error(update, context):
 Helpers
 ######################################################################################################################################################
 """
+
+
+def get_home_menu_buttons(context):
+    """
+    Return Keyboard buttons for the home menu
+    """
+
+    keyboard = [[]]
+
+    for address in context.user_data['nodes'].keys():
+        keyboard.append([InlineKeyboardButton(address, callback_data='node_details-' + address)])
+
+    keyboard.append([InlineKeyboardButton('Add Node', callback_data='add_node')])
+
+    return keyboard
+
+
+def show_detail_menu(update, context):
+    """
+    Show detail buttons for selected address
+    """
+
+    query = update.callback_query
+    address = context.user_data['selected_node_address']
+
+    text = "Node " + address + "\n"
+    text += "What do you want to do with that Node?"
+
+    keyboard = [[
+        InlineKeyboardButton('Delete Node', callback_data='confirm_node_deletion'),
+        InlineKeyboardButton('<< Back', callback_data='home')
+        ]]
+
+    # Modify message
+    query.edit_message_text(text, parse_mode='markdown', reply_markup=InlineKeyboardMarkup(keyboard))
 
 
 def expect(want, user_data):
@@ -209,7 +314,7 @@ def main():
     setup_existing_user(dispatcher=dispatcher)
 
     dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('cancel', show_home_menu_new_msg))
+    dispatcher.add_handler(CommandHandler('cancel', cancel))
     dispatcher.add_handler(CallbackQueryHandler(dispatch_query))
     dispatcher.add_handler(MessageHandler(Filters.text, plain_input))
 
