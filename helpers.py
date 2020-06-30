b9cd8a709e99826e9a8b65ffc1684109d738ca99
Paul to the Plessing <paul.plessing@block42.tech>
2020-06-30 18:49:56 +0700

    Improved User Interaction. Split Files. User blocked bot detection

---
+++ helpers.py
@@ -0,0 +1,223 @@
+import requests
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, TelegramError
+
+from constants import *
+
+"""
+######################################################################################################################################################
+Helpers
+######################################################################################################################################################
+"""
+
+
+def show_home_menu_new_msg(context, chat_id):
+    """
+    Send a new message with the home menu
+    """
+
+    keyboard = get_home_menu_buttons()
+    text = 'I am your Terra Node Bot. 🤖\nClick *MY NODES* to get information about the Terra Nodes you monitor!'
+    try_message(context=context, chat_id=chat_id, text=text, reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+def show_my_nodes_menu_new_msg(context, chat_id):
+    """
+    Show My Nodes Menu
+    """
+
+    user_data = context.user_data if context.user_data else context.job.context['user_data']
+
+    keyboard = get_my_nodes_menu_buttons(user_data=user_data)
+    text = 'Click an address from the list below or add a node:' if len(keyboard) > 2 else 'You do not monitor any ' \
+                                                                                        'Terra Nodes yet.\nAdd a Node!'
+
+    try_message(context=context, chat_id=chat_id, text=text, reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+def get_home_menu_buttons():
+    """
+    Return Keyboard buttons for the My Nodes menu
+    """
+
+    keyboard = [[InlineKeyboardButton('📡 MY NODES', callback_data='my_nodes')]]
+
+    return keyboard
+
+
+def get_my_nodes_menu_buttons(user_data):
+    """
+    Return Keyboard buttons for the My Nodes menu
+    """
+
+    keyboard = [[]]
+
+    for address in user_data['nodes'].keys():
+        keyboard.append([InlineKeyboardButton("📡 " + address, callback_data='node_details-' + address)])
+
+    keyboard.append([InlineKeyboardButton('➕ ADD NODE', callback_data='add_node'),
+                     InlineKeyboardButton('⬅️ BACK', callback_data='home')])
+
+    return keyboard
+
+
+def show_detail_menu(update, context):
+    """
+    Show detail buttons for selected address
+    """
+
+    query = update.callback_query
+    address = context.user_data['selected_node_address']
+
+    text = 'Node: *' + address + '*\n' + \
+           'Status: *' + NODE_STATUSES[context.user_data['nodes'][address]['status']] + '*\n' + \
+           'Jailed: *' + str(context.user_data['nodes'][address]['jailed']) + '*\n' + \
+           'Delegator Shares: *' + str(int(float(context.user_data['nodes'][address]['delegator_shares']))) + '*\n\n'
+
+    text += "What do you want to do with that Node?"
+
+    keyboard = [[
+        InlineKeyboardButton('➖ DELETE NODE', callback_data='confirm_node_deletion'),
+        InlineKeyboardButton('⬅️ BACK', callback_data='my_nodes')
+        ]]
+
+    # Modify message
+    query.edit_message_text(text, parse_mode='markdown', reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+def try_message(context, chat_id, text, reply_markup=None):
+    """
+    Send a message to a user.
+    """
+
+    if context.job and not context.job.enabled:
+        return
+
+    try:
+        context.bot.send_message(chat_id, text, parse_mode='markdown', reply_markup=reply_markup)
+    except TelegramError as e:
+        if 'bot was blocked by the user' in e.message:
+            print("Telegram user " + str(chat_id) + " blocked me; removing him from the user list")
+            del context.dispatcher.user_data[chat_id]
+            del context.dispatcher.chat_data[chat_id]
+            del context.dispatcher.persistence.user_data[chat_id]
+            del context.dispatcher.persistence.chat_data[chat_id]
+
+            # Somehow session.data does not get updated if all users block the bot.
+            # That makes problems on bot restart. That's why we delete the file ourselves.
+            if len(context.dispatcher.persistence.user_data) == 0:
+                if os.path.exists("storage/session.data"):
+                    os.remove("storage/session.data")
+            context.job.enabled = False
+            context.job.schedule_removal()
+        else:
+            print("Got Error\n" + str(e) + "\nwith telegram user " + str(chat_id))
+
+
+def get_validator(address):
+    """
+    Return json of desired validator node
+    """
+
+    if DEBUG:
+        # Get local validator file
+        response = requests.get('http://localhost:8000/validators.json')
+        nodes = response.json()
+        # Get the right node
+        node = next(filter(lambda node: node['operator_address'] == address, nodes['result']), None)
+        return node
+    else:
+        response = requests.get('https://lcd.terra.dev/staking/validators/' + address)
+        if response.status_code == 200:
+            node = response.json()
+            return node['result']
+        else:
+            return None
+
+
+def is_node_catching_up():
+    url = get_node_status_endpoint()
+    response = requests.get(url=url)
+    if response.status_code != 200:
+        return True
+
+    status = response.json()
+    return status['result']['sync_info']['catching_up']
+
+
+def get_node_block_height():
+    """
+    Return block height of your Terra Node
+    """
+
+    while True:
+        response = requests.get(url=get_node_status_endpoint())
+        if response.status_code == 200:
+            break
+
+    status = response.json()
+    return status['result']['sync_info']['latest_block_height']
+
+
+def is_price_feed_healthy(address):
+    """
+    Check whether price feed is working properly
+    """
+
+    prevotes = get_price_feed_prevotes(address)
+
+    if prevotes is None:
+        return False
+
+    for prevote in prevotes['result']:
+        if int(prevote['submit_block']) < int(prevotes['height']) - 10:
+            return False
+
+    return True
+
+
+def get_price_feed_prevotes(address):
+    """
+    Return the current prevotes oracle json
+    """
+
+    if DEBUG:
+        # Get local prevotes file
+        response = requests.get('http://localhost:8000/prevotes.json')
+        return response.json()
+    else:
+        response = requests.get('https://lcd.terra.dev/oracle/voters/' + address + '/prevotes')
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+
+def get_governance_proposals():
+    """
+    Return all governance proposals
+    """
+
+    while True:
+        response = requests.get(url=get_governance_proposal_endpoint())
+        if response.status_code == 200:
+            break
+
+    governance_proposals = response.json()
+    return governance_proposals['result']
+
+
+def get_node_status_endpoint():
+    """
+    Return the endpoint for block height checks
+    """
+
+    return 'http://localhost:8000/status.json' if DEBUG else 'http://' + NODE_IP + ':26657/status'
+
+
+def get_governance_proposal_endpoint():
+    """
+    Return the endpoint of governance proposals
+    """
+
+    return 'http://localhost:8000/governance_proposals.json' if DEBUG else 'https://lcd.terra.dev/gov/proposals'
+
--- terra_node_bot.py
+++ terra_node_bot.py
@@ -1,39 +1,14 @@
 import atexit
-import json
-import os
-import logging
 import re
 import subprocess
-from datetime import datetime
 
-import requests
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.error import BadRequest
 from telegram.ext import Updater, PicklePersistence, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, \
-    CallbackContext, run_async
+    run_async
 
-"""
-######################################################################################################################################################
-Static and Environment Variables
-######################################################################################################################################################
-"""
-
-DEBUG = bool(os.environ['DEBUG'] == 'True') if 'DEBUG' in os.environ else False
-TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-
-# Set NODE_IP depending on mode (if None, certain node health jobs are not executed)
-if DEBUG:
-    NODE_IP = 'localhost'
-elif 'NODE_IP' in os.environ and os.environ['NODE_IP']:
-    NODE_IP = os.environ['NODE_IP']
-else:
-    NODE_IP = None
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-NODE_STATUSES = ["Unbonded", "Unbonding", "Bonded"]
+from constants import *
+from helpers import *
+from jobs import *
 
 """
 ######################################################################################################################################################
@@ -118,7 +93,7 @@ def cancel(update, context):
     """
 
     context.user_data['expected'] = None
-    show_home_menu_new_msg(context=context, chat_id=update.effective_chat.id)
+    show_my_nodes_menu_new_msg(context=context, chat_id=update.effective_chat.id)
 
 
 @run_async
@@ -137,6 +112,8 @@ def dispatch_query(update, context):
 
     if data == 'home':
         call = show_home_menu_edit_msg
+    elif data == 'my_nodes':
+        call = show_my_nodes_menu_edit_msg
     elif data == 'add_node':
         call = add_node
     elif re.match('node_details', data):
@@ -186,25 +163,25 @@ def error(update, context):
     logger.warning('Update "%s" caused error: %s', update, context.error)
 
 
-def show_home_menu_new_msg(context, chat_id):
-    """
-    Send a new message with the home menu
-    """
-
-    user_data = context.user_data if context.user_data else context.job.context['user_data']
-
-    keyboard = get_home_menu_buttons(user_data=user_data)
-    text = 'I am your Terra Node Bot. 🤖\nChoose an address from the list below or add one:'
-    context.bot.send_message(chat_id, text, reply_markup=InlineKeyboardMarkup(keyboard))
-
-
 def show_home_menu_edit_msg(update, context):
     """
     Edit current message with the home menu
     """
 
-    keyboard = get_home_menu_buttons(user_data=context.user_data)
-    text = 'I am your Terra Node Bot. 🤖\nChoose an address from the list below or add one:'
+    keyboard = get_home_menu_buttons()
+    text = 'I am your Terra Node Bot. 🤖\nClick *MY NODES* to get information about the Terra Nodes you monitor!'
+    query = update.callback_query
+    query.edit_message_text(text, reply_markup=InlineKeyboardMarkup(keyboard), parse_mode='markdown')
+
+
+def show_my_nodes_menu_edit_msg(update, context):
+    """
+    Show My Nodes Menu
+    """
+
+    keyboard = get_my_nodes_menu_buttons(user_data=context.user_data)
+    text = 'Click an address from the list below or add a node:' if len(keyboard) > 2 else 'You do not monitor any ' \
+                                                                                        'Terra Nodes yet.\nAdd a Node!'
     query = update.callback_query
     query.edit_message_text(text, reply_markup=InlineKeyboardMarkup(keyboard))
 
@@ -243,7 +220,7 @@ def handle_add_node(update, context):
     context.user_data['nodes'][address]['delegator_shares'] = node['delegator_shares']
 
     context.bot.send_message(update.effective_chat.id, 'Got it! 👌')
-    return show_home_menu_new_msg(context=context, chat_id=update.effective_chat.id)
+    return show_my_nodes_menu_new_msg(context=context, chat_id=update.effective_chat.id)
 
 
 def node_details(update, context):
@@ -289,395 +266,7 @@ def delete_node(update, context):
     text = "❌ Node address got deleted! ❌\n" + address
     query.answer(text)
     query.edit_message_text(text)
-    show_home_menu_new_msg(context=context, chat_id=update.effective_chat.id)
-
-
-"""
-######################################################################################################################################################
-Helpers
-######################################################################################################################################################
-"""
-
-
-def get_home_menu_buttons(user_data):
-    """
-    Return Keyboard buttons for the home menu
-    """
-
-    keyboard = [[]]
-
-    for address in user_data['nodes'].keys():
-        keyboard.append([InlineKeyboardButton(address, callback_data='node_details-' + address)])
-
-    keyboard.append([InlineKeyboardButton('Add Node', callback_data='add_node')])
-
-    return keyboard
-
-
-def show_detail_menu(update, context):
-    """
-    Show detail buttons for selected address
-    """
-
-    query = update.callback_query
-    address = context.user_data['selected_node_address']
-
-    text = 'Node: *' + address + '*\n' + \
-           'Status: *' + NODE_STATUSES[context.user_data['nodes'][address]['status']] + '*\n' + \
-           'Jailed: *' + str(context.user_data['nodes'][address]['jailed']) + '*\n' + \
-           'Delegator Shares: *' + str(int(float(context.user_data['nodes'][address]['delegator_shares']))) + '*\n\n'
-
-    text += "What do you want to do with that Node?"
-
-    keyboard = [[
-        InlineKeyboardButton('Delete Node', callback_data='confirm_node_deletion'),
-        InlineKeyboardButton('<< Back', callback_data='home')
-        ]]
-
-    # Modify message
-    query.edit_message_text(text, parse_mode='markdown', reply_markup=InlineKeyboardMarkup(keyboard))
-
-
-def get_validator(address):
-    """
-    Return json of desired validator node
-    """
-
-    if DEBUG:
-        # Get local validator file
-        response = requests.get('http://localhost:8000/validators.json')
-        nodes = response.json()
-        # Get the right node
-        node = next(filter(lambda node: node['operator_address'] == address, nodes['result']), None)
-        return node
-    else:
-        response = requests.get('https://lcd.terra.dev/staking/validators/' + address)
-        if response.status_code == 200:
-            node = response.json()
-            return node['result']
-        else:
-            return None
-
-
-def is_node_catching_up():
-    url = get_node_status_endpoint()
-    response = requests.get(url=url)
-    if response.status_code != 200:
-        return True
-
-    status = response.json()
-    return status['result']['sync_info']['catching_up']
-
-
-def get_node_block_height():
-    """
-    Return block height of your Terra Node
-    """
-
-    while True:
-        response = requests.get(url=get_node_status_endpoint())
-        if response.status_code == 200:
-            break
-
-    status = response.json()
-    return status['result']['sync_info']['latest_block_height']
-
-
-def is_price_feed_healthy(address):
-    """
-    Check whether price feed is working properly
-    """
-
-    prevotes = get_price_feed_prevotes(address)
-
-    if prevotes is None:
-        return False
-
-    for prevote in prevotes['result']:
-        if int(prevote['submit_block']) < int(prevotes['height']) - 10:
-            return False
-
-    return True
-
-
-def get_price_feed_prevotes(address):
-    """
-    Return the current prevotes oracle json
-    """
-
-    if DEBUG:
-        # Get local prevotes file
-        response = requests.get('http://localhost:8000/prevotes.json')
-        return response.json()
-    else:
-        response = requests.get('https://lcd.terra.dev/oracle/voters/' + address + '/prevotes')
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return None
-
-
-def get_governance_proposals():
-    """
-    Return all governance proposals
-    """
-
-    while True:
-        response = requests.get(url=get_governance_proposal_endpoint())
-        if response.status_code == 200:
-            break
-
-    governance_proposals = response.json()
-    return governance_proposals['result']
-
-
-def get_node_status_endpoint():
-    """
-    Return the endpoint for block height checks
-    """
-
-    return 'http://localhost:8000/status.json' if DEBUG else 'http://' + NODE_IP + ':26657/status'
-
-
-def get_governance_proposal_endpoint():
-    """
-    Return the endpoint of governance proposals
-    """
-
-    return 'http://localhost:8000/governance_proposals.json' if DEBUG else 'https://lcd.terra.dev/gov/proposals'
-
-
-"""
-######################################################################################################################################################
-Jobs
-######################################################################################################################################################
-"""
-
-
-def node_checks(context):
-    """
-    Periodic checks of various node stats
-    """
-
-    check_node_status(context)
-    check_price_feeder(context)
-    check_governance_proposals(context)
-    if NODE_IP:
-        check_node_catch_up_status(context)
-        check_node_block_height(context)
-
-
-def check_node_status(context):
-    """
-    Check all added Terra Nodes for any changes.
-    """
-
-    chat_id = context.job.context['chat_id']
-    user_data = context.job.context['user_data']
-
-    # Flag to show home buttons or not
-    message_sent = False
-
-    # List to delete entries after loop
-    delete_addresses = []
-
-    # Iterate through all keys
-    for address in user_data['nodes'].keys():
-        remote_node = get_validator(address=address)
-        local_node = user_data['nodes'][address]
-
-        if remote_node is None:
-            text = 'Node is not active anymore! 💀' + '\n' + \
-                   'Address: ' + address + '\n\n' + \
-                   'Please enter another Node address.'
-
-            delete_addresses.append(address)
-
-            # Send message
-            context.bot.send_message(chat_id, text)
-            message_sent = True
-            continue
-
-        # Check which node fields have changed
-        changed_fields = [field for field in ['status', 'jailed', 'delegator_shares'] if
-                          local_node[field] != remote_node[field]]
-
-        # Check if there are any changes
-        if len(changed_fields) > 0:
-            text = 'Node: *' + address + '*\n' + \
-                   'Status: *' + NODE_STATUSES[local_node['status']]
-            if 'status' in changed_fields:
-                text += '* ➡️ *' + NODE_STATUSES[remote_node['status']]
-            text += '*\nJailed: *' + str(local_node['jailed'])
-            if 'jailed' in changed_fields:
-                text += '* ➡️ *' + str(remote_node['jailed'])
-            text += '*\nDelegator Shares: *' + str(int(float(local_node['delegator_shares'])))
-            if 'delegator_shares' in changed_fields:
-                text += '* ➡️ *' + str(int(float(remote_node['delegator_shares'])))
-            text += '*'
-
-            # Update data
-            local_node['status'] = remote_node['status']
-            local_node['jailed'] = remote_node['jailed']
-            local_node['delegator_shares'] = remote_node['delegator_shares']
-
-            # Send message
-            context.bot.send_message(chat_id, text, parse_mode='markdown')
-            message_sent = True
-
-    for address in delete_addresses:
-        del user_data['nodes'][address]
-
-    if message_sent:
-        show_home_menu_new_msg(context=context, chat_id=chat_id)
-
-
-def check_price_feeder(context):
-    """
-    Check Prevotes to make sure Price Feeder still works
-    """
-
-    chat_id = context.job.context['chat_id']
-    user_data = context.job.context['user_data']
-
-    for address in user_data['nodes'].keys():
-        if 'is_price_feed_healthy' not in user_data:
-            user_data['is_price_feed_healthy'] = True
-
-        is_price_feed_currently_healthy = is_price_feed_healthy(address)
-        if user_data['is_price_feed_healthy'] == True and not is_price_feed_currently_healthy:
-            user_data['is_price_feed_healthy'] = False
-            text = 'Price feed is not healthy anymore! 💀' + '\n' + \
-                   'Address: ' + address
-            context.bot.send_message(chat_id, text)
-            show_home_menu_new_msg(context, chat_id=chat_id)
-        elif user_data['is_price_feed_healthy'] == False and is_price_feed_currently_healthy:
-            user_data['is_price_feed_healthy'] = True
-            text = 'Price feed is healthy again! 👌' + '\n' + \
-                   'Address: ' + address + '\n'
-            context.bot.send_message(chat_id, text)
-            show_home_menu_new_msg(context, chat_id=chat_id)
-
-        
-def check_node_catch_up_status(context):
-    """
-    Check if node is some blocks behind with catch up status
-    """
-
-    chat_id = context.job.context['chat_id']
-    user_data = context.job.context['user_data']
-
-    if 'is_catching_up' not in user_data:
-        user_data['is_catching_up'] = False
-
-    is_currently_catching_up = is_node_catching_up()
-    if user_data['is_catching_up'] == False and is_currently_catching_up:
-        user_data['is_catching_up'] = True
-        text = 'The Node is behind the latest block height and catching up! 💀 ' + '\n' + \
-               'IP: ' + NODE_IP + '\n' + \
-               'Current block height: ' + get_node_block_height() + '\n\n' + \
-               'Please check your Terra Node immediately!'
-        context.bot.send_message(chat_id, text)
-        show_home_menu_new_msg(context=context, chat_id=chat_id)
-    elif user_data['is_catching_up'] == True and not is_currently_catching_up:
-        user_data['is_catching_up'] = False
-        text = 'The node caught up to the latest block height again! 👌' + '\n' + \
-               'IP: ' + NODE_IP + '\n' + \
-               'Current block height: ' + get_node_block_height()
-        context.bot.send_message(chat_id, text)
-        show_home_menu_new_msg(context=context, chat_id=chat_id)
-        
-
-def check_node_block_height(context):
-    """
-    Make sure the block height increases
-    """
-
-    chat_id = context.job.context['chat_id']
-    user_data = context.job.context['user_data']
-
-    block_height = get_node_block_height()
-
-    # Check if block height got stuck
-    if 'block_height' in user_data and block_height <= user_data['block_height']:
-        # Increase stuck count to know if we already sent a notification
-        user_data['block_height_stuck_count'] += 1
-    else:
-        # Check if we have to send a notification that the Height increases again
-        if 'block_height_stuck_count' in user_data and user_data['block_height_stuck_count'] > 0:
-            text = 'Block height is increasing again! 👌' + '\n' + \
-                   'IP: ' + NODE_IP + '\n' + \
-                   'Block height now at: ' + block_height + '\n'
-            context.bot.send_message(chat_id, text)
-            user_data['block_height_stuck_count'] = -1
-        else:
-            user_data['block_height_stuck_count'] = 0
-
-    # Set current block height
-    user_data['block_height'] = block_height
-
-    # If it just got stuck send a message
-    if user_data['block_height_stuck_count'] == 1:
-        text = 'Block height is not increasing anymore! 💀' + '\n' + \
-               'IP: ' + NODE_IP + '\n' + \
-               'Block height stuck at: ' + block_height + '\n\n' + \
-               'Please check your Terra Node immediately!'
-        context.bot.send_message(chat_id, text)
-
-    # Show buttons if there were changes or block height just got (un)stuck
-    # Stuck count:
-    # 0 == everthings alright
-    # 1 == just got stuck
-    # -1 == just got unstuck
-    # > 1 == still stuck
-
-    if user_data['block_height_stuck_count'] == 1 or user_data['block_height_stuck_count'] == -1:
-        show_home_menu_new_msg(context=context, chat_id=chat_id)
-
-
-def check_governance_proposals(context):
-    """
-    Monitoring related to governance proposals
-    """
-
-    check_new_goverance_proposal(context)
-
-
-def check_new_goverance_proposal(context):
-    """
-    Notify the user if there's a new governance proposals
-    """
-
-    chat_id = context.job.context['chat_id']
-    user_data = context.job.context['user_data']
-
-    governance_proposals = get_governance_proposals()
-    governance_proposals_count = len(governance_proposals)
-
-    if 'governance_proposals_count' not in user_data:
-        user_data['governance_proposals_count'] = governance_proposals_count
-
-    new_proposals_count = governance_proposals_count - user_data['governance_proposals_count']
-    for i in range(new_proposals_count):
-        current_proposal = governance_proposals[user_data['governance_proposals_count'] + i]
-
-        voting_start_time = datetime.strptime(current_proposal['voting_start_time'][:-4], "%Y-%m-%dT%H:%M:%S.%f")
-        voting_end_time = datetime.strptime(current_proposal['voting_end_time'][:-4], "%Y-%m-%dT%H:%M:%S.%f")
-
-        text = 'A new governance proposal got submitted! 📣\n\n' + \
-               '*Title:*\n' + current_proposal['content']['value']['title'] + '\n' + \
-               '*Type:*\n' + current_proposal['content']['type'] + '\n' + \
-               '*Description:*\n' + current_proposal['content']['value']['description'] + '\n\n' + \
-               '*Voting Start Time:* ' + voting_start_time.strftime("%A %B %d, %H:%M") + ' UTC\n' + \
-               '*Voting End Time:* ' + voting_end_time.strftime("%A %B %d, %H:%M") + ' UTC\n\n' + \
-               'Make sure to vote on this governance proposal until *' + voting_end_time.strftime("%A %B %d, %H:%M") + ' UTC*!'
-
-        context.bot.send_message(chat_id, text, parse_mode='markdown')
-
-    user_data['governance_proposals_count'] = governance_proposals_count
-
-    if new_proposals_count:
-        show_home_menu_new_msg(context=context, chat_id=chat_id)
+    show_my_nodes_menu_new_msg(context=context, chat_id=update.effective_chat.id)
 
 
 """
